7eee80a06714402113f75263752e662af0492f22
Rafael <rafaelserafinsousa@hotmail.com>
2025-09-10 17:27:45 -0300

    aula 28

---
+++ spring/telas/telas/src/main/java/com/telas/app/controllers/TelasController.java
@@ -0,0 +1,16 @@
+package com.telas.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TelasController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping("/cadastrar")
+    public String cadastrar(){
+        return "cadastrar";
+    }
+}
